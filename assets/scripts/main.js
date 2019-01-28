f434acd58a0289f39994a7dc9ff08a9913958d83
Anish-Agnihotri <contact@anishagnihotri.com>
2019-01-28 18:26:46 -0500

    Added output to html, pricing broken

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,5 @@
 /* Predefined vars */
-let apiKey = ''; /* Etherscan API */
+let apiKey = 'T2IH6VJHTEYT3VCZKE495BETGNQZC5235Z'; /* Etherscan API */
 let uniswapAddr = '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'; /* Uniswap mainnet contract */
 
 let pools = []; /* Array to hold list of currency pools */
@@ -36,13 +36,38 @@ $.ajax({
                         type: "GET",
                         success: function (result) {
                             if (result.status !== '0') {
-                                console.log(result.result[0].contractAddress);
+                                let tokenAddress = (result.result[0].contractAddress);
+                                let tokenSymbol = (result.result[0].tokenSymbol);
+                                let tokenName = (result.result[0].tokenName);
+
+                                getPrice(tokenSymbol);
+
+                                $('#attach').append('<p>' + tokenAddress + ' - ' + tokenName + ' - ' + tokenSymbol + ' - ' + tokenPrice + '</p>');
                             }
                         }
                     })
                     counter++;
-                }, 300);
+                }, 600);
             }
+        }
+
+        function getPrice(symbol) {
+            $.ajax({
+                url: 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=' + symbol,
+                type: "GET",
+                success: function (result) {
+                    storeValue(result[symbol]); // why is jquery so disgusting
+                }
+            })
+        }
+
+        function storeValue(pricing) {
+            if (pricing == undefined) {
+                pricing = 'no number found';
+            } else {
+                tokenPrice = pricing;
+            }
+            return tokenPrice;
         }
     }
 })
