8f43008efec48b0b5b51bd1b00cdbdde3a26d18c
Anish-Agnihotri <contact@anishagnihotri.com>
2019-01-28 14:43:45 -0500

    Hitting ratelimit on request

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,41 @@
 /* Predefined vars */
-let apiKey = 'sneaky sneaky im not pushing my api key to github ;)'; /* Etherscan API */
+let apiKey = ''; /* Etherscan API */
 let uniswapAddr = '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'; /* Uniswap mainnet contract */
 
-let pools = []; /* Array to hold list of Uniswap pools */
+let pools = []; /* Array to hold list of currency pools */
 
 $.ajax({
     url: 'http://api.etherscan.io/api?module=account&action=txlistinternal&address=' + uniswapAddr + '&startblock=0&sort=asc&apikey=' + apiKey,
     type: "GET",
-    success: function(result) {
-        let maxPools = result.result.length;
-        for (i = 0; i < maxPools; i++) {
-            pools.push(result.result[i].contractAddress)
+    success: function (result) {
+
+        let poolContracts = [];
+
+        for (i = 0; i < result.result.length; i++) {
+            poolContracts.push(result.result[i].contractAddress)
         }
+
+        let poolContractsCleanup = poolContracts.filter(
+            function (a, v) {
+                if (!this[a]) {
+                    this[a] = 1;
+                    return a!=='';
+                }
+            }, {}
+        );
+
+        poolContractsCleanup.forEach(function(address) {
+            $.ajax({
+                url: 'http://api.etherscan.io/api?module=account&action=tokentx&address=' + address + '&startblock=0&endblock=999999999&sort=asc&apikey=' + apiKey,
+                type: "GET",
+                success: function (result) {
+                    if (status !== '0') {
+                        pools.push(result.result[0].contractAddress);
+                    }
+                }
+            })
+        });
+        
         console.log(pools);
     }
 })
